c509d1bb41a0547634d911009505a14d42b8548b
jiyong.yoon <jyyoon0615@naver.com>
2023-09-10 23:47:49 +0900

    test 격리 - 3
    - Embedded Mode로 h2 db 사용하기 (직접 Bean 등록)

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,13 +2,16 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import javax.sql.DataSource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -24,6 +27,19 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	// JVM에 Embedded Mode DB를 사용하도록 세팅
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1"); // DB_CLOSE_DELAY=-1 = 임베디드 모드에서는 데이터베이스 커넥션이 모두 끊어지면 데이터베이스가 종료되는데, 그것을 방지하는 설정.
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 
 }
